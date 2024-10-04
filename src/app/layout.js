21fdb8ceeeb1645fa01f7c2cc6ecc2bf2731fd66
Ahmed Elsakaa <ahmedelsakaa75@gmail.com>
2024-10-04 23:37:13 +0300

    Add Dark Mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Providers from "./Providers";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,9 +24,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        >
+          <Providers>
         <Navbar />
         {children}
+        </Providers>
       </body>
     </html>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
-import MenuItem from './MenuItem';
-import {AiFillHome} from 'react-icons/ai'
-import {BsFillInfoCircleFill} from 'react-icons/bs'
-import Link from 'next/link';
+import React from "react";
+import MenuItem from "./MenuItem";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import Link from "next/link";
+import DarkModeSwitch from "./DarkModeSwitch";
 
 export default function Navbar() {
-  return<>
-  <div className="flex items-center justify-between p-3 max-w-6xl mx-auto">
+  return (
+    <>
+      <div className="flex items-center justify-between p-3 max-w-6xl mx-auto">
+        <div className="flex items-center gap-4">
+         
+          <Link href={"/"} className="flex gap-1 items-center">
+            <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-md">
+              IMDb
+            </span>
+            <span className="text-xl hidden sm:inline">Clone</span>
+          </Link>
+        </div>
 
-
-  <Link href={'/'} className="flex gap-1 items-center">
-    <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDB</span>
-    <span className='text-xl hidden sm:inline'>Clone</span>
-  </Link>
-
-  <div className="flex gap-4">
-    <MenuItem title="Home" address="/" Icon={AiFillHome} />
-    <MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill} />
-    
-  </div>
-
-  </div>
-  </>
+        <div className="flex gap-5 items-center">
+          <MenuItem title="Home" address="/" Icon={AiFillHome} />
+          <MenuItem
+            title="About"
+            address="/about"
+            Icon={BsFillInfoCircleFill}
+          />
+           <DarkModeSwitch />
+        </div>
+      </div>
+    </>
+  );
 }
