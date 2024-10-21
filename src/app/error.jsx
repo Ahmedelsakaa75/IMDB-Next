16b090d32049c79db4a5cbbcf8fb50e538e54200
Ahmed Elsakaa <ahmedelsakaa75@gmail.com>
2024-10-21 20:28:08 +0300

    Final enhancment

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,18 +1,20 @@
 'use client'
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react';
 
-export default function Error({error, reset}) {
-  
-  useEffect(()=>{
+export default function Error({ error, reset }) {
+  useEffect(() => {
     console.log(error);
-    
-  } ,[error])
-  
-    return (
-    <><div className="text-center mt-20 flex content-center justify-center flex-col">
-    <h1 className="text-3xl m-32">Something went wrong</h1>
-    <button className='border p-3 rounded-xl w-1/2 m-auto hover:bg-amber-500 hover:text-black transition' onClick={()=>{reset()}}>Try Again</button>
+  }, [error]);
+
+  return (
+    <div className="text-center mt-20 flex content-center justify-center flex-col">
+      <h1 className="text-3xl m-32">Something went wrong</h1>
+      <button
+        className="border p-3 rounded-xl w-1/2 m-auto hover:bg-amber-500 hover:text-black transition"
+        onClick={() => reset()}
+      >
+        Try Again
+      </button>
     </div>
-    </>
-  )
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,30 @@
-'use client'
 import Results from '@/components/Results';
-import React, { Suspense } from 'react'
+import React, { Suspense } from 'react';
+
 const api_key = process.env.API_KEY;
 
-export default  function Home({searchParams}) {
-const genre = searchParams.genre || 'fetchTrending'
-  
-    async function handleGettingMovies (){
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'fetchTrending';
 
-      const res = await fetch(`https://api.themoviedb.org/3${
-      genre === 'fetchTopRated'? '/movie/top_rated' : '/trending/all/week'  
-      }?api_key=${api_key}&language=en-US&page=1`, {next:{ revalidate: 0 }} );
-      const data = await res.json()
-      if(!res.ok){
-        throw new Error('Failed to fetch data')
-      }
-      return movies = data.results
-    }
-    handleGettingMovies()
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
+    }?api_key=${api_key}&language=en-US&page=1`,
+    { next: { revalidate: 0 } }
+  );
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  const data = await res.json();
+  const movies = data.results;
+
   return (
-     <Suspense fallback={<div>Loading...</div>}>
     <div className="flex">
-      <Results movies={movies} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Results movies={movies} />
+      </Suspense>
     </div>
-    </Suspense>  
-  )
+  );
 }
