719090b0ac46d300f0338c59cebe9b151f02bc73
Ahmed Elsakaa <ahmedelsakaa75@gmail.com>
2024-10-21 20:04:53 +0300

    Making home as a client side component

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,23 @@
+'use client'
 import Results from '@/components/Results';
 import React, { Suspense } from 'react'
 const api_key = process.env.API_KEY;
 
-export default async function Home({searchParams}) {
+export default  function Home({searchParams}) {
 const genre = searchParams.genre || 'fetchTrending'
   
-    
-    const res = await fetch(`https://api.themoviedb.org/3${
-    genre === 'fetchTopRated'? '/movie/top_rated' : '/trending/all/week'  
-    }?api_key=${api_key}&language=en-US&page=1`, {next:{ revalidate: 0 }} );
-    const data = await res.json()
-    if(!res.ok){
-      throw new Error('Failed to fetch data')
-    }
-    const movies = data.results
+    async function handleGettingMovies (){
 
+      const res = await fetch(`https://api.themoviedb.org/3${
+      genre === 'fetchTopRated'? '/movie/top_rated' : '/trending/all/week'  
+      }?api_key=${api_key}&language=en-US&page=1`, {next:{ revalidate: 0 }} );
+      const data = await res.json()
+      if(!res.ok){
+        throw new Error('Failed to fetch data')
+      }
+      return movies = data.results
+    }
+    handleGettingMovies()
   return (
      <Suspense fallback={<div>Loading...</div>}>
     <div className="flex">
